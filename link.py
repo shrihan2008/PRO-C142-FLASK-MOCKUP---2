6b5683af82c26a397943d9031977b9d384d7b079
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-09-28 17:55:49 +0530

    Add files via upload

--- link.py
+++ link.py
@@ -25,8 +25,9 @@ def get_article():
 @app.route("/liked-article", methods=["POST"])
 def liked_article():
     article = all_articles[0]
-    all_articles = all_articles[1:]
+    all_articles = all_articles[1:]  
     liked_articles.append(article)
+    all_articles.pop(0)
     return jsonify({
         "status": "success"
     }), 201
@@ -36,20 +37,20 @@ def unliked_article():
     article = all_articles[0]
     all_articles = all_articles[1:]
     not_liked_articles.append(article)
+    all_articles.pop(0)
+   
     return jsonify({
         "status": "success"
     }), 201
 
 def populararticles():
     article_data=[]
-    for article in output:
-        d={
-           "title": article[12], 
-           "eventType": article[3], 
-           "contentId": article[4] ,
-           
-        }
-        article_data.append(d)
+    article_data = { 
+    "url": all_articles[0][11],
+     "title": all_articles[0][12], 
+     "text": all_articles[0][13], 
+     "lang": all_articles[0][14], 
+     "total_events": all_articles[0][15] }
     return jsonify({
         'data':article_data,
         'status':'win'
