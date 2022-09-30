ef93b40748b183ccfb0c8cc74bc1d89fa3dd38b0
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-09-30 12:29:55 +0530

    Add files via upload

--- link.py
+++ link.py
@@ -14,48 +14,26 @@ not_liked_articles = []
 
 
 app = Flask(__name__)
+@app.route("/get-article") 
+def get_article(): 
+    article_data = {
+         "url": all_articles[0][11], 
+         "title": all_articles[0][12], 
+         "text": all_articles[0][13], 
+         "lang": all_articles[0][14], 
+         "total_events": all_articles[0][15] 
+         } 
+    return jsonify({ "data": article_data, "status": "success" })
+    
+@app.route("/liked-article", methods=["POST"]) 
+def liked_article(): 
+    article = all_articles[0] 
+    liked_articles.append(article) 
+    all_articles.pop(0) 
+    return jsonify({ "status": "success" })
 
-@app.route("/get-article")
-def get_article():
-    return jsonify({
-        "data": all_articles[0],
-        "status": "success"
-    })
-
-@app.route("/liked-article", methods=["POST"])
-def liked_article():
-    article = all_articles[0]
-    all_articles = all_articles[1:]  
-    liked_articles.append(article)
-    all_articles.pop(0)
-    return jsonify({
-        "status": "success"
-    }), 201
-
-@app.route("/unliked-article", methods=["POST"])
-def unliked_article():
-    article = all_articles[0]
-    all_articles = all_articles[1:]
-    not_liked_articles.append(article)
-    all_articles.pop(0)
-   
-    return jsonify({
-        "status": "success"
-    }), 201
-
-def populararticles():
-    article_data=[]
-    article_data = { 
-    "url": all_articles[0][11],
-     "title": all_articles[0][12], 
-     "text": all_articles[0][13], 
-     "lang": all_articles[0][14], 
-     "total_events": all_articles[0][15] }
-    return jsonify({
-        'data':article_data,
-        'status':'win'
-
-             })   
-
-if __name__ == "__main__":
-  app.run()
+def not_liked_article(): 
+    article = all_articles[0] 
+    not_liked_articles.append(article) 
+    all_articles.pop(0) 
+    return jsonify({ "status": "success" })
